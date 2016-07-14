46c58e286bb120182c813714ce77f737480e7971
bsevgeny@gmail.com <papakita2009>
2016-07-14 10:08:27 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.fluttercode.datafactory.impl.DataFactory;
+
+/**
+ * Created by Evegeny on 14/07/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        DataFactory dataFactory = new DataFactory();
+    }
+}
